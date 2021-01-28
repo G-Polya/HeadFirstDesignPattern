a94cfb6c62899088cf0f2e0fdd8d7a84b9247184
DESKTOP-60TM4II\KimHeeSu <wlskaka4@gmail.com>
2021-01-28 16:31:11 +0900

    templatemethod pattern

---
+++ src/templatemethod/barista/CaffeineBeverageWithHook.java
@@ -0,0 +1,30 @@
+package templatemethod.barista;
+
+import javax.sound.midi.Soundbank;
+
+public abstract class CaffeineBeverageWithHook {
+    final void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        if(customWantsCondiments()) {
+            addCondiments();
+        }
+    }
+
+    abstract void brew();
+
+    abstract void addCondiments();
+
+    void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+
+    boolean customWantsCondiments() {
+        return true;
+    }
+}
